73f45b23913f965a69f1d5eeab67c3c082a516bb
Li, Chanping （李婵娉） <Chanping.Li@elekta.com>
2021-05-19 11:18:33 +0800

    update installer data

--- Huya_Live_Barrage/pyintaller_runner.py
+++ Huya_Live_Barrage/pyintaller_runner.py
@@ -3,6 +3,7 @@ import PyInstaller.__main__
 PyInstaller.__main__.run([
     'huya_live_barrage_v2.py',
     '--onefile',
+    '--exclude-module=tkinter',
     r'-iFacebook_icon-icons.com_66805.ico',
     # '--windowed'
 ])
